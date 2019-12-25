62dfcec5ee4f955651b5452ae8091bdbff87bbab
baosongshan <baosongshan2006@163.com>
2019-12-25 21:38:34 +0800

    add avl.Remove rbtree概念

--- 55/C++/Test12_19_AVL/Test.cpp
+++ 55/C++/Test12_19_AVL/Test.cpp
@@ -29,9 +29,11 @@ public:
 	AVLTree() : root(nullptr)
 	{}
 public:
-	bool Insert(const Type &x);
+	bool Insert(const Type &x);  //接口
+	bool Remove(const Type &key);
 protected:
-	bool Insert(AVLNode<Type> *&t, const Type &x);
+	bool Insert(AVLNode<Type> *&t, const Type &x); //
+	bool Remove(AVLNode<Type> *&t, const Type &key); //
 protected:
 	void RotateR(AVLNode<Type> *&ptr)
 	{
@@ -51,7 +53,27 @@ protected:
 	}
 	void RotateRL(AVLNode<Type> *&ptr)
 	{
-		//???
+		AVLNode<Type> *subL = ptr;
+		AVLNode<Type> *subR = ptr->rightChild;
+		ptr = subR->leftChild;
+
+		subR->leftChild = ptr->rightChild;
+		ptr->rightChild = subR;
+		//subR->bf
+		if(ptr->bf >= 0)
+			subR->bf = 0;
+		else
+			subR->bf = 1;
+
+		subL->rightChild = ptr->leftChild;
+		ptr->leftChild = subL;
+		//subL->bf
+		if(ptr->bf <= 0)
+			subL->bf = 0;
+		else
+			subL->bf = -1;
+
+		ptr->bf = 0;
 	}
 	void RotateLR(AVLNode<Type> *&ptr)
 	{
@@ -74,6 +96,8 @@ protected:
 			subR->bf = 0;
 		else
 			subR->bf = 1;
+
+		ptr->bf = 0;
 	}
 private:
 	AVLNode<Type> *root;
@@ -131,11 +155,11 @@ bool AVLTree<Type>::Insert(AVLNode<Type> *&t, const Type &x)
 		else
 			pr->bf++;
 
-		if(pr->bf == 0)
+		if(pr->bf == 0)  //在矮子树上插入节点
 			break;
-		if(pr->bf==1 || pr->bf==-1)
+		if(pr->bf==1 || pr->bf==-1) //增加了子树的高度
 			p = pr;
-		else
+		else   // |bf| == 2 需要平衡调整
 		{
 			if(pr->bf > 0)
 			{
@@ -146,8 +170,8 @@ bool AVLTree<Type>::Insert(AVLNode<Type> *&t, const Type &x)
 				}	
 				else             //      >
 				{
-					cout<<"RotateRL"<<endl;
-					//RotateRL(pr);
+					//cout<<"RotateRL"<<endl;
+					RotateRL(pr);
 				}
 			}
 			else
@@ -166,7 +190,6 @@ bool AVLTree<Type>::Insert(AVLNode<Type> *&t, const Type &x)
 			break;
 		}
 	}
-
 	if(st.empty())
 	{
 		t = pr;
@@ -183,9 +206,109 @@ bool AVLTree<Type>::Insert(AVLNode<Type> *&t, const Type &x)
 	return true;
 }
 
-void main()
+template<typename Type>
+bool AVLTree<Type>::Remove(const Type &key)
 {
-	int ar[] = {16, 34, 20, 7, 11, 9, 26, 18, 14, 15};
+	return Remove(root, key);
+}
+template<typename Type>
+bool AVLTree<Type>::Remove(AVLNode<Type> *&t, const Type &key)
+{
+	stack<AVLNode<Type>*> st;
+	//查找节点
+	AVLNode<Type> *p = t, *pr = nullptr;
+	while(p != nullptr)
+	{
+		if(key == p->data)
+			break;
+
+		pr = p;
+		st.push(pr);
+
+		if(key < p->data)
+			p = p->leftChild;
+		else
+			p = p->rightChild;
+	}
+	if(p == nullptr)
+		return false;
+
+	AVLNode<Type> *q;
+	if(p->leftChild!=nullptr &&p->rightChild!=nullptr)
+	{
+		pr = p;
+		st.push(pr);
+		q = p->leftChild;
+		
+		while(q->rightChild != nullptr)
+		{
+			pr = q;
+			st.push(pr);
+			q = q->rightChild;
+		}
+		p->data = q->data;
+		p = q;
+	}
+
+	if(p->leftChild != nullptr)
+		q = p->leftChild;
+	else
+		q = p->rightChild;
+
+	if(pr == nullptr)
+	{
+		t = q;
+	}
+	else
+	{
+		//删除节点
+		if(pr->leftChild == p)
+			pr->leftChild = q;
+		else
+			pr->rightChild = q;
+
+		//调整bf
+		while(!st.empty())
+		{
+			if(pr->leftChild == q)
+				pr->bf++;
+			else
+				pr->bf--;
+
+			if(pr->bf==1 ||pr->bf==-1)
+				break;
+			if(pr->bf == 0)
+				q = pr;
+			else
+			{
+				//旋转调整平衡
+				if(pr->bf > 0)
+					q = pr->rightChild;
+				else
+					q = pr->leftChild;
+
+				//1 bf==0
+				if(q->bf == 0) //   /  \  >  <
+				{}
+				//2 bf符号相同
+
+				//3 bf符号不同
+				
+			}
+
+		}
+
+	}
+	
+	delete p;
+	return true;
+}
+
+
+int main()
+{
+	int ar[] = {16, 3, 7, 11, 9, 26, 18, 14, 15};
+	//int ar[] = {4, 2, 6, 1, 3, 5, 15, 7, 16, 14};
 	//int ar[] = {16, 8, 10};
 	int n = sizeof(ar) / sizeof(int);
 
@@ -194,4 +317,6 @@ void main()
 	{
 		avl.Insert(ar[i]);
 	}
+	avl.Remove(11);
+	return 0;
 }
